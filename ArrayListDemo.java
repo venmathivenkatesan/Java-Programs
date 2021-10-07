d388b50150ff5394b9998a3c4641c2b8dd4dfe11
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2021-10-07 20:22:04 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class ArrayListDemo 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList <Integer> intarr = new ArrayList <Integer>();
+
+        intarr.add(1);
+		intarr.add(3);
+		intarr.add(3);
+		intarr.add(7);
+		intarr.add(5);
+		intarr.add(6);
+		intarr.add(9);
+		intarr.add(6);
+		intarr.add(4);
+		intarr.add(1);
+		System.out.println(intarr);
+	}
+}
